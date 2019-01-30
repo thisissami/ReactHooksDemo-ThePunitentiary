9d0d7ee4a27b85dafebeb63d4f771f049b6271f8
thisissami <sami@rubikon.io>
2019-01-29 22:03:28 -0800

    used better variable names

--- src/components/thePunitentiary.js
+++ src/components/thePunitentiary.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import PunList from './finished/punList';
 import { useCurrentPun } from '../hooks/useCurrentPun';
-import {
-  getGoodPuns_Sync,
-  subscribeToThePunitentiary,
-  unsubscribeFromAll
-} from '../apis/thePunApi';
+import { getGoodPuns_Sync } from '../apis/thePunApi';
 
 const ThePunitentiary = () => {
   // grab the puns
   const puns = getGoodPuns_Sync();
   // use our custom hook!
-  const [currentPun, setCurrentPun] = useCurrentPun(puns);
+  const [currentPunIndex, setCurrentPunIndex] = useCurrentPun(puns);
 
   // continuously feed the *very willing* audience more puns
 
@@ -20,7 +16,7 @@ const ThePunitentiary = () => {
   return (
     <>
       <h1>The Punitentiary</h1>
-      <PunList puns={puns} onPunClick={setCurrentPun} currentPun={currentPun} />
+      <PunList puns={puns} onPunClick={setCurrentPunIndex} currentPun={currentPunIndex} />
     </>
   );
 
